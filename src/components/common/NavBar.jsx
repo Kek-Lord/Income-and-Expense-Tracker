f21a932e8d7eddb827ea86e9d7bb52ca2cb914f6
Matthew <matthew.tomlinson27@gmail.com>
2025-09-23 20:56:47 +0100

    Updated UI, implemented basic persistence

--- src/components/common/NavBar.jsx
+++ src/components/common/NavBar.jsx
@@ -1,13 +1,52 @@
-
-
 export default function Navbar() {
+  const currentPath = window.location.pathname;
+
   return (
-    <nav className="w-full p-0 flex justify-center items-center">
+    <nav className=" w-64 bg-gray-950 text-white flex flex-col p-4"
+    style={{height: "100vh"}}>
       {/* Logo here */}
-      
-      <div className="flex p-2 bg-gray-800 w-full justify-center gap-8 text-2xl">
-        <a href="/" className="">Dashboard</a>
-        <a href="/accounts" className="">Accounts</a>
+      {/* Links Below */}
+
+      <div className="flex flex-col gap-4 text-lg">
+        <a
+          href="/"
+          className={`px-4 py-2 rounded hover:bg-gray-700 transition-colors ${
+            currentPath === "/" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}
+        >
+          Dashboard
+        </a>
+        <a
+          href="/accounts"
+          className={`px-4 py-2 rounded hover:bg-gray-700 transition-colors ${
+            currentPath === "/accounts" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}
+          >
+          Accounts
+        </a>
+        <a
+          href="/insights"
+          className={`px-4 py-2 rounded hover:bg-gray-700 transition-colors ${
+            currentPath === "/insights" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}
+          >
+          Insights
+        </a>
+        <a
+          href="/reports"
+          className={`px-4 py-2 rounded hover:bg-gray-700 transition-colors ${
+            currentPath === "/reports" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}>
+          Reports
+        </a>
+        <a
+          href="/settings"
+          className={`px-4 py-2 rounded hover:bg-gray-700 transition-colors ${
+            currentPath === "/settings" ? "bg-gray-700" : "hover:bg-gray-700"
+          }`}
+          >
+          Settings
+        </a>
       </div>
     </nav>
   );
